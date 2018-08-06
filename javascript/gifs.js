e8527936cbf126a06e0b2f2ca7ef8c28a54fab0e
CB6 <melissa.j.womack@gmail.com>
2018-08-05 22:21:02 -0500

    Made .js file framework

--- javascript/gifs.js
+++ javascript/gifs.js
@@ -1,16 +1,40 @@
-//Click even to add button
 
-$("#addFood").click(function () {
 
-    newButtonText = $("#food-input").val();
+$(document).ready(function () {
+    //Click even to add button
 
-    newButton = $("<button class='buttons'>" + newButtonText + "</button>");
+    $("#addFood").click(function () {
 
-    $(".button-container").append(newButton);
+        newButtonText = $("#food-input").val();
+        newButton = $("<button class='buttons'>" + newButtonText + "</button>");
+        newButtonTextVal = toLowerCase(trim(newButtonText));
+        newButton.attr("data-food", newButtonTextVal);
+        $(".button-container").append(newButton);
 
+    });
+
+    $("button").click(function () {
+
+        var food = $(this).attr("data-food");
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=dGfiE2c3mUZUhNtQ99HWn3qzCDW1GTir";
+
+        $.ajax({ URL: queryUrl, method: 'GET' }).then(function (response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var newDiv = $("<div>");
+                var newImgDiv = $("<img>");
+                var newRatingDiv = $("<p>");
+
+                newImgDiv.attr("src", results[i].images.fixed_height.url);
+                newRatingDiv.text("Rating: " + results[i].rating);
+
+                newDiv.append(newRatingDiv);
+                newDiv.append(ImgDiv);
+                gifContainer.prepend(newDiv);
+
+            }
+
+        });
+    });
 });
-
-// $(document).ready(function () {
-
-
-// });
